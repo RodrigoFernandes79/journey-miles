c0d7d22e2abf7f4e5b32eb8e2a67364143302b4d
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2025-01-19 19:36:42 -0300

    test method listAllClients with scenarios when validates passes and not passes in ClientServiceTest class

--- src/test/java/com/br/rodrigo/jornadamilhas/services/ClientServiceTest.java
+++ src/test/java/com/br/rodrigo/jornadamilhas/services/ClientServiceTest.java
@@ -2,12 +2,15 @@ package com.br.rodrigo.jornadamilhas.services;
 
 import com.br.rodrigo.jornadamilhas.domains.client.Client;
 import com.br.rodrigo.jornadamilhas.domains.client.ClientDataInput;
+import com.br.rodrigo.jornadamilhas.domains.client.ListClientDataOutput;
 import com.br.rodrigo.jornadamilhas.domains.user.User;
+import com.br.rodrigo.jornadamilhas.exceptions.DataNotFoundException;
 import com.br.rodrigo.jornadamilhas.exceptions.ExistingDataException;
 import com.br.rodrigo.jornadamilhas.exceptions.PasswordNotEqualsException;
 import com.br.rodrigo.jornadamilhas.repositories.ClientRepository;
 import com.br.rodrigo.jornadamilhas.repositories.CommentRepository;
 import com.br.rodrigo.jornadamilhas.repositories.UserRepository;
+import org.hibernate.mapping.Collection;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,7 +21,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -44,6 +51,7 @@ class ClientServiceTest {
     private ClientDataInput clientDataInput;
     @Mock
     private Client client;
+
 
     @Test
     @DisplayName("Should Return a client when validate pass")
@@ -89,5 +97,34 @@ class ClientServiceTest {
         //Act / when && Assertion / then
         assertThrows(PasswordNotEqualsException.class, () -> clientService.createClient(clientDataInput));
 
+    }
+
+    @Test
+    @DisplayName("Should return Client List when validate pass")
+    void listAllClients_scenario01() {
+        //Arrange / Given
+        List<Client> clientList = new ArrayList<>();
+        clientList.add(client);
+        given(clientRepository.findAll()).willReturn(clientList);
+        List<ListClientDataOutput> dataOutputList = clientList.stream()
+                .map(ListClientDataOutput::new).collect(Collectors.toList());
+
+        //Act / when
+        List<ListClientDataOutput> result = clientService.listAllClients();
+        // Assertion / then
+        assertEquals(1, result.size());
+        assertEquals(clientList.size(), result.size());
+        assertEquals(clientList.get(0).getEmail(), result.get(0).email());
+        assertEquals(dataOutputList.get(0).username(), result.get(0).username());
+    }
+
+    @Test
+    @DisplayName("Should return exception when validate not pass")
+    void listAllClients_scenario02() {
+        //Arrange / Given
+        given(clientRepository.findAll()).willReturn(Collections.emptyList());
+
+        //Act / when && Assertion / then
+        assertThrows(DataNotFoundException.class, () -> clientService.listAllClients());
     }
 }
