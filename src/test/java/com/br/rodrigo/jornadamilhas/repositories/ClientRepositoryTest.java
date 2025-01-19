80fc457f962bb00f00dce27556a5972aab5b0141
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2025-01-19 15:58:04 -0300

    test ClientRepositoryTest queries

---
+++ src/test/java/com/br/rodrigo/jornadamilhas/repositories/ClientRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.br.rodrigo.jornadamilhas.repositories;
+
+import com.br.rodrigo.jornadamilhas.domains.address.Address;
+import com.br.rodrigo.jornadamilhas.domains.client.Client;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+class ClientRepositoryTest {
+    @Autowired
+    private ClientRepository clientRepository;
+
+    private Client client;
+    private Address address;
+
+    @BeforeEach
+    void beforeEachMethod() {
+        //Arrange / Given
+        address = new Address("anu street", "any district", "34532348", "343",
+                "403", "any city", "any State");
+        client = new Client(null, "inaldinho", "email@email.com",
+                "81995458654", "58696505000", "inaldinho.jpeg", "1312", null, address, null);
+    }
+
+    @Test
+    @DisplayName("Deve Retornar um objeto ao buscar por email")
+    void findByEmail() {
+        //Arrange / given
+        clientRepository.save(client);
+        //Act / When
+        Optional<Client> clientEntity = clientRepository.findByEmail(client.getEmail());
+        //Assert / Then
+        assertNotNull(clientEntity);
+        assertEquals(client.getEmail(), clientEntity.get().getEmail());
+    }
+
+    @Test
+    @DisplayName("Deve Retornar um objeto ao buscar por cpf")
+    void findByCpf() {
+        //Arrange / given
+        clientRepository.save(client);
+        //Act / When
+        Optional<Client> clientEntity = clientRepository.findByCpf(client.getCpf());
+        //Assert / Then
+        assertNotNull(clientEntity);
+        assertEquals(client.getCpf(), clientEntity.get().getCpf());
+    }
+}
